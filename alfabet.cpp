1692ad12cce453d77c77d59646b63341c4ea64ee
PW310595 <147628637+PW310595@users.noreply.github.com>
2023-11-20 22:33:12 +0100

    alfabet.cpp

--- alfabet.cpp
+++ alfabet.cpp
@@ -12,3 +12,24 @@ int main() {
 
     return 0;
 }
+
+
+==========================================================================
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            std::cout << "Aaaa Grrrrrr" << std::endl;
+        } else if (i % 3 == 0) {
+            std::cout << "Aaaa" << std::endl;
+        } else if (i % 5 == 0) {
+            std::cout << "Grrrrrr" << std::endl;
+        } else {
+            std::cout << i << std::endl;
+        }
+    }
+
+    return 0;
+}
