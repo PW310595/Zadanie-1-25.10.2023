b54e35096d344ab3199a26a8c36f55eac4c36eb0
PW310595 <147628637+PW310595@users.noreply.github.com>
2024-01-16 23:23:24 +0100

    maxmin.cpp

---
+++ maxmin.cpp
@@ -0,0 +1,32 @@
+void Losowo3(std::vector<T>& tablica, int roz) {
+
+    for (int i = 0; i < roz; i++) {
+        tablica.push_back(rand() % 100);
+    }
+    for (int i = 0; i < tablica.size(); i++) {
+        std::cout << tablica[i] << " ";
+    }
+
+    int min = INT_MAX;
+    for (int j = 0; j < tablica.size(); j++) {
+        if (tablica[j] < min) {
+            min = tablica[j];
+        }
+    }
+    std::cout << " MIN: " << min << std::endl;
+
+    int max = INT_MIN;
+    for (int k = 0; k < tablica.size(); k++) {
+        if (tablica[k] > max) {
+            max = tablica[k];
+        }
+    }
+    std::cout << " MAX: " << max << std::endl;
+}
+
+
+   int n3;
+    std::cout << "Podaj n3" << std::endl;
+    std::cin >> n3;
+    std::vector<int> Wektor3;
+    Losowo3(Wektor3, n3);
