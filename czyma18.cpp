4036777439ed42efa5eb25fce312ada0ee8b3071
Paweł Wróblewski <147628637+PW310595@users.noreply.github.com>
2024-11-12 19:17:00 +0100

    Update czyma18.cpp

--- czyma18.cpp
+++ czyma18.cpp
@@ -1,39 +1,132 @@
-#include <iostream>
+##################################### Zadanie1
 
-int main() {
-    int dzien, miesiac, rok;
-    
-    std::cout << "Podaj dzien urodzenia: ";
-    std::cin >> dzien;
-    
-    std::cout << "Podaj miesiac urodzenia: ";
-    std::cin >> miesiac;
-    
-    std::cout << "Podaj rok urodzenia: ";
-    std::cin >> rok;
-    
-    int aktualnyDzien = 20;
-    int aktualnyMiesiac = 11;
-    int aktualnyRok = 2023;
-    
-    if((
-	(miesiac==1 || miesiac==3 || miesiac==5 || miesiac==7 || miesiac==8 || miesiac==10 || miesiac==12) && (dzien<32 && dzien>0))
-	 || 
-	 (
-	(miesiac==4 || miesiac==6 || miesiac==9 || miesiac==11) && (dzien<31 && dzien>0))
-	||
-	 ((miesiac==2) && (dzien<29 && dzien>0))){
-    	if (aktualnyRok - rok > 18 || (aktualnyRok - rok == 18 && aktualnyMiesiac > miesiac) || (aktualnyRok - rok == 18 && aktualnyMiesiac == miesiac && aktualnyDzien >= dzien))
-    {
-        std::cout << "Masz 18 lat!" << std::endl;
-    }
-    else
-    {
-        std::cout << "Nie masz 18 lat." << std::endl;
-    }
-	} else {
-		std::cout << "Blad danych" << std::endl;
+class SimpleSimulation: public Simulation
+{
+protected:
+	
+	virtual void Next() {
+
 	}
-    
-    
-}
+
+	virtual void Init() {
+
+		Animal* animal = new Animal();
+		this->safari.Add(animal);
+	}
+
+};
+
+##################################### Zadanie2
+
+#pragma once
+using namespace std;
+class Vector2D {
+private:
+
+	double x, y;
+
+public:
+	//2 konstruktor
+	Vector2D(double xx, double yy) {
+		x = xx;
+		y = yy;
+	}
+
+	//2a unarny
+
+	Vector2D operator-() {
+		return Vector2D(x*(-1), y*(-1));
+	}
+	//2a binarny
+
+	Vector2D operator-(const Vector2D drugiwektor) {
+		return Vector2D(x - drugiwektor.x, y - drugiwektor.y);
+
+	}
+
+	//2b
+
+	Vector2D operator+=(const Vector2D drugiwektor) {
+		x += drugiwektor.x;
+		y += drugiwektor.y;
+
+		return Vector2D(x, y);
+
+	}
+
+	//2c
+
+	Vector2D operator*(const double skalar) {
+		return Vector2D(x*skalar, y*skalar);
+	}
+
+	//2d
+
+	void normalize() {
+		double dlugosc = sqrt(x*x +  y*y);
+		if (dlugosc > 0) {
+			x = x / dlugosc;
+			y = x / dlugosc;
+		}
+	}
+
+	//2e
+
+	void Show() {
+		cout << " x i y: "<< x << " " << y << endl;
+	}
+
+	//2f
+
+	double dist(const Vector2D drugiwektor){
+
+		double xx = x - drugiwektor.x;
+		double yy = y - drugiwektor.y;
+		double dist = sqrt(xx*xx + yy*yy);
+		return dist;
+	}
+
+};
+
+##################################### Zadanie3
+
+#pragma once
+#include "safari.h"
+#include <iostream>
+#include "vector2d.h"
+class Animal
+{
+protected:
+	//3a
+	Vector2D pozycja;
+	//3c
+	Vector2D kierunek;
+	//3d
+	double predkosc;
+public:
+	void Show()
+	{
+		std::cout << "metoda Show klasy Animal\n";
+	}
+	
+
+	void Do(Safari& safari)
+	{
+		std::cout << "metoda Do klasy Animal\n";
+	}
+
+	//3b
+	Vector2D GetPos() const {
+		return pozycja;
+	}
+
+	//3e
+
+	Animal(Vector2D poz, Vector2D kie, double pre) {
+		pozycja = poz;
+		kierunek = kie;
+		predkosc = pre;
+	}
+
+
+};
